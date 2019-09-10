1646cd8d43f90fb1fc2ef44f389b2675bacfe40d
Pranav Lamkhade <pranavlamkhade12@gmail.com>
2019-09-10 15:47:13 +0530

    First Commit

---
+++ myapp/src/app/app.component.ts
@@ -0,0 +1,76 @@
+import { Component } from '@angular/core';
+import * as CanvasJS from './canvasjs-2.3.2/canvasjs.min';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  chart;
+  chartOptions = {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: { 
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+	};
+  chartOptions2 = {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: { 
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "line",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+	};
+  chartOptions3 = {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: { 
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "spline",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+	};
+  navigate(){
+    
+  }
+}
